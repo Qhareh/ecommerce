beb1aa000ed95d0571fef1d6dd73193f2e636118
Cindy <ngongucynthia@gmail.com>
2022-09-20 01:42:40 +0300

    creating cart cookie

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,21 +1,44 @@
 var updatebtns = document.getElementsByClassName('update-cart')
 
-for(var i = 0; i < updatebtns.length; i++){
-    updatebtns[i].addEventListener('click', function(){
+for (var i = 0; i < updatebtns.length; i++) {
+    updatebtns[i].addEventListener('click', function () {
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('ProductId:', productId, 'Action:', action)
 
         console.log('USER:', user)
-        if(user == 'AnonymousUser'){
-            console.log('Not logged in')
-        }else{
-           updateUserOrder(productId, action)
+        if (user == 'AnonymousUser') {
+            addCookieItem(productId, action)
+        } else {
+            updateUserOrder(productId, action)
         }
     })
 }
 
-function updateUserOrder(productId, action){
+function addCookieItem(productId, action) {
+    console.log('User not logged in')
+    if (action == 'add') {
+        if (cart[productId] == undefined) {
+
+            cart[productId] = { 'quantity': 1 }
+        }
+        else {
+            cart[productId]['quantity'] += 1
+        }
+    }
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('delete item')
+            delete cart[productId];
+        }
+    }
+    console.log('Cart:', cart)
+
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+}
+
+function updateUserOrder(productId, action) {
     console.log('User is logged in, sending data..')
 
     var url = '/update_item/'
@@ -25,14 +48,14 @@ function updateUserOrder(productId, action){
             'Content-Type': 'application/json',
             'X-CSRFToken': csrftoken,
         },
-        body: JSON.stringify({'productId' : productId, 'action': action})
+        body: JSON.stringify({ 'productId': productId, 'action': action })
     })
 
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-       console.log('Data :', data)
-       location.reload()
-    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log('Data :', data)
+            location.reload()
+        })
 }
